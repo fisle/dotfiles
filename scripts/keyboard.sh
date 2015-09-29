f347fcc1d1516b042bc2d11731939be448ac9297
Petri Immonen <petri.immonen@scandinavianoutdoor.com>
2015-09-29 11:56:15 +0300

    Remove caps lock rebind

--- scripts/keyboard.sh
+++ scripts/keyboard.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 setxkbmap fi
-xmodmap -e "keycode 49 = less greater less greater bar brokenbar bar"
-xmodmap -e "remove Lock = Caps_Lock"
-xmodmap -e "Caps_Lock = Scroll_Lock"
 setxkbmap -option "nbsp:none"
+xmodmap -e "keycode 49 = less greater less greater bar brokenbar bar"
+# xmodmap -e "remove Lock = Caps_Lock"
+# xmodmap -e "Caps_Lock = Scroll_Lock"
 xsetroot -cursor_name left_ptr
