e1d266a3cf8cd78fcd1e4c2f625c5906b4107bff
Petri Immonen <petri.immonen@scandinavianoutdoor.com>
2015-10-20 10:14:31 +0300

    Primary displays

--- scripts/oma.sh
+++ scripts/oma.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 xrandr --output VIRTUAL1 --off --output DP2 --off --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output eDP1 --auto --output VGA1 --off
 nitrogen --restore
+xrandr --output eDP1 --primary
--- scripts/work.sh
+++ scripts/work.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 1920x120 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --mode 1920x1200 --pos 0x0 --rotate normal --output DP2 --off
 nitrogen --restore
+xrandr --output HDMI1 --primary
