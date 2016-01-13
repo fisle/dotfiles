0bcb925af7a78de81a38dc6c5913e5c43b21f6b0
fisle <petri@fisle.eu>
2016-01-13 10:35:09 +0200

    Update music script

--- scripts/music.sh
+++ scripts/music.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-DATA=$(mpc playlist --format "%artist% - %title%\`%position%" | grep -F -f <(mpc playlist | fzf) | cut -d '`' -f2)
+# DATA=$(mpc playlist --format "%artist% - %title%\`%position%" | grep -F -f <(mpc playlist | fzf) | cut -d '`' -f2)
+DATA=$(mpc playlist --format "%artist% - %title%\`%position%" | fzf --reverse | cut -d '`' -f2)
 echo $DATA
 if [ ! -z "$DATA" ]
 then
