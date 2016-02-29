e65e9f3f112ad9752044986a34cce3232fa54411
fisle <petri@fisle.eu>
2016-02-29 20:01:39 +0200

    Rework primary monitor script

--- scripts/primary.sh
+++ scripts/primary.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-if (xrandr | grep "HDMI1 disconnected"); then
-    xrandr --output eDP1 --primary --output HDMI1 --off
+if (xrandr | grep "DP2-2 connected"); then
+    xrandr --output DP2-2 --auto --primary --output eDP1 --auto --right-of DP2-2
 else
-    xrandr --output HDMI1 --auto --primary --output eDP1 --auto --right-of HDMI1
+    xrandr --output eDP1 --primary --output DP2-2 --off
 fi
 nitrogen --restore
+i3-msg restart
