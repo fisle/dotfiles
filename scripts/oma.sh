c3007b607055f3bbd81e4d4c34166c3fc95f693f
fisle <petri@fisle.eu>
2016-10-17 08:59:22 +0300

    Fix display scripts

--- scripts/oma.sh
+++ scripts/oma.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP2 --off --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output eDP1 --auto --output VGA1 --off
-nitrogen --restore
+xrandr --output VIRTUAL1 --off --output DP2 --off --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output eDP1 --auto --output VGA1 --off --output DP2-2 --off
 xrandr --output eDP1 --primary
+nitrogen --restore
+i3-msg restart
--- scripts/work.sh
+++ scripts/work.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 1920x120 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --mode 1920x1200 --pos 0x0 --rotate normal --output DP2 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 1920x120 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --mode 1920x1200 --pos 0x0 --rotate normal --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+xrandr --output DP2-2 --primary
 nitrogen --restore
-xrandr --output HDMI1 --primary
+i3-msg restart
