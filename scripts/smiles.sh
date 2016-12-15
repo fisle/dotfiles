ed52b1ad664214a73068eae3ddd8154a3d7afda4
fisle <petri@fisle.eu>
2016-12-15 12:01:34 +0200

    Lujaa shellcheck ajoo

--- scripts/smiles.sh
+++ scripts/smiles.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 
-cd /tmp
+cd /tmp || exit
 LATESTGAJIM=$(curl https://eddykk.com/api/page/gajim | cut -d '"' -f2)
-wget https://eddykk.com/smiley/gajim/$LATESTGAJIM
-unzip $LATESTGAJIM
+wget https://eddykk.com/smiley/gajim/"$LATESTGAJIM"
+unzip "$LATESTGAJIM"
 filename=$(basename "$LATESTGAJIM")
 filename="${filename%.*}"
-gksudo mv $filename /usr/share/gajim/data/emoticons/
-rm $LATESTGAJIM
+gksudo mv "$filename" /usr/share/gajim/data/emoticons/
+rm "$LATESTGAJIM"
 
 
 LATESTPIDGIN=$(curl https://eddykk.com/api/page/pidgin | cut -d '"' -f2)
-wget https://eddykk.com/smiley/pidgin/$LATESTPIDGIN
-unzip $LATESTPIDGIN
+wget https://eddykk.com/smiley/pidgin/"$LATESTPIDGIN"
+unzip "$LATESTPIDGIN"
 filename=$(basename "$LATESTPIDGIN")
 filename="${filename%.*}"
-mv $filename ~/.purple/smileys/
-rm $LATESTPIDGIN
+mv "$filename" ~/.purple/smileys/
+rm "$LATESTPIDGIN"
 
 notify-send 'Smilies extracted' -u critical -t 5000
