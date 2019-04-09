e35caa3e2b605edc42fe2b076cd5a3530427b973
fisle <petri@fisle.eu>
2019-04-10 00:44:55 +0300

    Rework pulseaudio sink switcher
    Previous implementation didn't work on my new system.
    This should be a bit less prone to errors.

--- scripts/sink.sh
+++ scripts/sink.sh
@@ -1,23 +1,102 @@
 #!/bin/bash
-sinkinputs=$(pactl list short sink-inputs | cut -f1)
-current=$(cat ~/.sink)
-# sinks=$(pactl list short sinks | cut -f1)
-count=$(pactl list short sinks | wc -l)
-count=$(expr $count - 1)
+function get_sinks_short() {
+    pactl list short sinks | cut -f1
+}
+function get_inputs() {
+    pactl list short sink-inputs | cut -f1
+}
 
-if [ "$current" == "$count" ]; then
-    output=0
-else
-    output=$(expr $current + 1)
-fi
+function get_sink_name() {
+    sink="$1"
 
-pacmd set-default-sink "$output"
-echo "$output" > ~/.sink
+    if [ -z "$sink" ]; then
+        echo "No sink specified"
+        return
+    fi
 
-notify-send "$(pactl list sinks | grep device.description | head -$(expr $output + 1) | tail -1 | grep -oP '"\K[^"\047]+(?=["\047])')"
+    pactl list sinks | grep device.description | sed "$sink"'q;d' | grep -oP '"\K[^"\047]+(?=["\047])'
+}
 
-for sink in $sinkinputs
-do
-    pacmd move-sink-input "$sink" "$output"
-done
+function set_default_sink() {
+    output="$1"
 
+    if [ -z "$output" ]; then
+        echo "No output specified"
+        return
+    fi
+
+    pacmd set-default-sink "$output"
+    sink_name=$(get_sink_name "$((output + 1))")
+    printf "%s\\n%s" "$output" "$sink_name" > ~/.sink
+}
+
+function get_default_sink() {
+    head -n1 ~/.sink
+}
+
+function get_default_sink_pretty() {
+    name=$(tail -n1 ~/.sink)
+    echo "${name::35}"
+}
+
+function move_inputs() {
+    output="$1"
+
+    if [ -z "$output" ]; then
+        echo "No output specified"
+        return
+    fi
+
+    for sink in $(get_inputs)
+    do
+        pacmd move-sink-input "$sink" "$output"
+    done
+}
+
+function main() {
+    if [ ! -z "$1" ] && [ "$1" == "print" ]; then
+        get_default_sink_pretty
+        return
+    fi
+
+    current=$(get_default_sink)
+    sinks=$(get_sinks_short)
+
+    index=$(get_index "$sinks" "$current")
+
+    next_index=$((index + 1))
+    next_sink=$(echo "$sinks" | sed "$next_index"'q;d')
+
+    count=$(get_sinks_short | wc -l)
+
+    if [ "$next_index" -gt "$count" ]; then
+        next_sink=$(echo "$sinks" | sed '1q;d')
+    fi
+
+    set_default_sink "$next_sink"
+    move_inputs "$next_sink"
+
+    notify-send "Input changed: $(get_default_sink_pretty)"
+}
+
+function get_index() {
+    my_array="$1"
+    value="$2"
+
+    if [ -z "$my_array" ] || [ -z "$value" ]; then
+        echo "Not enough parameters"
+        return
+    fi
+
+    counter=1
+    for item in $my_array
+    do
+        if [ "$item" == "$value" ]; then
+            echo "$counter"
+        fi
+
+        (( counter+=1 ))
+    done
+}
+
+main "$@"
